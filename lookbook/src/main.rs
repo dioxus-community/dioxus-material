582d959a2aff86752e2daeb756ca2fbc21722677
Matt Hunzinger <matthunz2@gmail.com>
2023-10-24 04:45:42 -0400

    Update to latest lookbook

--- lookbook/src/main.rs
+++ lookbook/src/main.rs
@@ -1,62 +1,53 @@
 use dioxus::prelude::*;
 use dioxus_material::{use_theme, Button, TextButton, TextField};
-use lookbook::{preview, Look, LookBook};
+use lookbook::{preview, LookBook};
 
 #[preview]
-fn ButtonPreview(cx: Scope) -> Element {
-    let label = use_state(cx, || String::from("Filled Button"));
+fn ButtonPreview(
+    cx: Scope,
+    /// Label for the button.
+    #[lookbook(default = "Label")]
+    label: &'a str,
+) -> Element {
     let theme = use_theme(cx);
     let background_color = use_state(cx, || theme.primary_color.to_string());
 
-    render! {
-        Look {
-            name: "Button",
-            controls: render! {
-                TextField { label : "Label", value : label, onchange : move | event : FormEvent |
-                label.set(event.data.value.clone()) } TextField { label : "Background color", value :
-                background_color, onchange : move | event : FormEvent | background_color.set(event
-                .data.value.clone()) }
-            },
-            Button { background_color: background_color, onpress: |_| {}, &*** label }
-        }
-    }
+    render!(Button {
+        background_color: background_color,
+        onpress: |_| {},
+        label
+    })
 }
 
 #[preview]
-fn TextButtonPreview(cx: Scope) -> Element {
-    let label = use_state(cx, || String::from("Text Button"));
 
-    render!(
-        Look {
-            name: "TextButton",
-            controls: render!(
-                TextField { label : "Label", value : label, onchange : move | event : FormEvent |
-                label.set(event.data.value.clone()) }
-            ),
-            TextButton { onpress: |_| {}, &*** label }
-        }
-    )
+fn TextButtonPreview(
+    cx: Scope,
+    /// Label for the text button.
+    #[lookbook(default = "Label")]
+    label: &'a str,
+) -> Element {
+    render!(TextButton {
+        onpress: |_| {},
+        label
+    })
 }
 
+/// Text fields let users enter text into a UI.
 #[preview]
-fn TextFieldPreview(cx: Scope) -> Element {
+fn TextFieldPreview<'a>(
+    cx: Scope,
+    /// Label for the text field.
+    #[lookbook(default = "Label")]
+    label: &'a str,
+) -> Element<'a> {
     let value = use_state(cx, || String::from("Text Field"));
-    let label = use_state(cx, || String::from("Label"));
 
-    render!(
-        Look {
-            name: "TextField",
-            controls: render!(
-                TextField { label : "Label", value : label, onchange : move | event : FormEvent |
-                label.set(event.data.value.clone()) }
-            ),
-            TextField {
-                label: &**label,
-                value: value,
-                onchange: move |event: FormEvent| value.set(event.data.value.clone())
-            }
-        }
-    )
+    render!(TextField {
+        label: label,
+        value: value,
+        onchange: move |event: FormEvent| value.set(event.data.value.clone())
+    })
 }
 
 #[component]
@@ -77,7 +68,10 @@ fn Home(cx: Scope) -> Element {
 }
 
 fn app(cx: Scope) -> Element {
-    render!(LookBook { home: Home, previews: [ButtonPreview, TextButtonPreview, TextFieldPreview] })
+    render!(LookBook {
+        home: Home,
+        previews: [ButtonPreview, TextButtonPreview, TextFieldPreview]
+    })
 }
 
 fn main() {
